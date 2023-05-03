a7041c558100b77d3e3651c9561ffd1ff9766241
unknown <2124290043@ACADEMICO.IESB.BR>
2023-05-02 21:19:34 -0300

    ask

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
-import LoginForm from './components/LoginForm'
-import RegisterForm from './components/RegisterForm'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <Header />
-      <LoginForm />
     </div>
   )
 }
